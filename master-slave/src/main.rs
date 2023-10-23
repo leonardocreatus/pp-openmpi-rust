3383e569d111f85e8492f62eb30c2adb78470a60
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2023-10-23 19:52:09 -0300

    investigating deadlock

--- master-slave/src/main.rs
+++ master-slave/src/main.rs
@@ -2,9 +2,9 @@ use std::{fs, time::Instant};
 
 use image::io::Reader as ImageReader;
 use image::GenericImageView;
-use mpi::traits::{Communicator, Destination, Equivalence, Source};
+use mpi::traits::*;
 
-#[repr(C)]
+#[derive(Equivalence)]
 struct Histogram {
     r: [u32; 256],
     g: [u32; 256],
@@ -44,47 +44,11 @@ impl Histogram {
 
         Self { r, g, b }
     }
-
-    //pub fn to_bytes(&self) -> [u8; 768] {
-    //    let mut bytes = [0u32; 256 * 3];
-    //    bytes[0..256].clone_from_slice(&self.r);
-    //    bytes[256..512].clone_from_slice(&self.g);
-    //    bytes[512..768].clone_from_slice(&self.b);
-    //    bytes
-    //}
-
-    //pub fn from_bytes(bytes: &[u32; 768]) -> Self {
-    //    let mut r = [0u32; 256];
-    //    let mut g = [0u32; 256];
-    //    let mut b = [0u32; 256];
-
-    //    r.clone_from_slice(&bytes[0..256]);
-    //    g.clone_from_slice(&bytes[256..512]);
-    //    b.clone_from_slice(&bytes[512..]);
-
-    //    Self { r, g, b }
-    //}
 }
 
-unsafe impl Equivalence for Histogram {
-    type Out = mpi::datatype::UserDatatype;
-
-    fn equivalent_datatype() -> Self::Out {
-        mpi::datatype::UserDatatype::contiguous(768, &u32::equivalent_datatype())
-    }
-}
-
-#[repr(C)]
+#[derive(Equivalence)]
 struct Filename {
     name: [u8; 256],
-}
-
-unsafe impl Equivalence for Filename {
-    type Out = mpi::datatype::UserDatatype;
-
-    fn equivalent_datatype() -> Self::Out {
-        mpi::datatype::UserDatatype::contiguous(256, &u8::equivalent_datatype())
-    }
 }
 
 fn main() {
@@ -99,7 +63,9 @@ fn main() {
     if rank == 0 {
         // Mestre
         let start = Instant::now();
+
         let mut send_to = 1;
+        let mut hist = Histogram::new();
         for path in paths {
             let p = path.unwrap().path();
             let p_bytes = p.to_str().unwrap().as_bytes();
@@ -111,9 +77,14 @@ fn main() {
             world.process_at_rank(send_to).send(&filename);
             println!("done sending");
 
-            send_to = (send_to + 1) % size;
-            if send_to == 0 {
-                send_to += 1;
+            send_to += 1;
+            if send_to == size {
+                send_to = 1;
+                //    for _ in 1..size {
+                //        let (msg, _status) = world.any_process().receive::<Histogram>();
+                //        println!("received image from: {}", _status.source_rank());
+                //        hist = hist.join(msg);
+                //    }
             }
         }
 
@@ -123,12 +94,6 @@ fn main() {
             world.process_at_rank(rank).send(&filename);
         }
 
-        let mut hist = Histogram::new();
-        for i in 0..3500 {
-            let (msg, _status) = world.any_process().receive::<Histogram>();
-            println!("image: {i}");
-            hist = hist.join(msg);
-        }
         let end = Instant::now();
         println!("reds: {:?}", hist.r);
         println!("greens: {:?}", hist.g);
@@ -141,7 +106,9 @@ fn main() {
         // Trabalhador
         loop {
             println!("{rank}: waiting");
-            let (Filename { name: msg }, _status) = world.process_at_rank(0).receive::<Filename>();
+            let (filename, _status) = world.process_at_rank(0).receive::<Filename>();
+            println!("{rank}: waiting");
+            let Filename { name: msg } = filename;
 
             let zero = msg
                 .iter()
@@ -154,18 +121,18 @@ fn main() {
                 break;
             }
 
-            let mut hist = Histogram::new();
-            let filename = String::from_utf8_lossy(&msg[0..zero]).to_string();
-            println!("{rank}: processing: {filename}");
-            let img: image::DynamicImage = ImageReader::open(&filename).unwrap().decode().unwrap();
-            for (_, _, pixel) in img.pixels() {
-                hist.r[pixel[0] as usize] += 1;
-                hist.g[pixel[1] as usize] += 1;
-                hist.b[pixel[2] as usize] += 1;
-            }
+            //let mut hist = Histogram::new();
+            //let filename = String::from_utf8_lossy(&msg[0..zero]).to_string();
+            //println!("{rank}: processing: {filename}");
+            //let img: image::DynamicImage = ImageReader::open(&filename).unwrap().decode().unwrap();
+            //for (_, _, pixel) in img.pixels() {
+            //    hist.r[pixel[0] as usize] += 1;
+            //    hist.g[pixel[1] as usize] += 1;
+            //    hist.b[pixel[2] as usize] += 1;
+            //}
 
-            world.process_at_rank(0).send(&hist);
-            println!("{rank}: done processing: {filename}");
+            ////world.process_at_rank(0).send(&hist);
+            //println!("{rank}: done processing: {filename}");
         }
     }
 }
